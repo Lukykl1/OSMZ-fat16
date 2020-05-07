6c721a929f752460e5d9f3423061588d3571d241
Lukykl1 <lukykl1@gmail.com>
2020-05-07 13:50:39 +0200

    added direcotry change, io command line

--- fat.c
+++ fat.c
@@ -7,15 +7,18 @@
 PartitionTable pt[4];
 Fat16BootSector bs;
 FILE *in;
-
+Fat16Entry *actual_dir = NULL;
+int entries;
 unsigned short *fat;
 void get_filename(Fat16Entry entry, char *filename, bool is_subdirectory);
 bool print_file(Fat16Entry entry, int *file_size);
 int get_start_of_root_dir();
 void move_to_cluster(int cluster);
-void read_file(char *filename);
+void read_file(char *filename, bool save);
 void init_fs();
 void dir();
+void chdir();
+void load_initial_dir();
 
 void get_filename(Fat16Entry entry, char *filename, bool is_subdirectory)
 {
@@ -33,8 +36,14 @@ void get_filename(Fat16Entry entry, char *filename, bool is_subdirectory)
   {
     filename[start_of_ext++] = '.';
   }
-  memcpy(filename + start_of_ext, entry.ext, sizeof(char) * 3);
-  filename[12] = 0;
+  if (!is_subdirectory)
+  {
+    memcpy(filename + start_of_ext, entry.ext, sizeof(char) * 3);
+  }
+  else
+  {
+    memset(filename + start_of_ext, 0, 12 - start_of_ext);
+  }
 }
 bool print_file(Fat16Entry entry, int *file_size)
 {
@@ -81,24 +90,21 @@ int get_start_of_root_dir()
 }
 bool get_entry_by_name(char *filename, Fat16Entry *entry)
 {
-  int start_of_root_dir = get_start_of_root_dir();
-  fseek(in, start_of_root_dir, SEEK_SET); //set from start
-  for (int i = 0; i < bs.root_dir_entries; i++)
+  for (int i = 0; i < entries; i++)
   {
-    fread(entry, sizeof(Fat16Entry), 1, in);
-    if (entry->filename[0] != 0x00)
+
+    char entry_filename[13];
+    memset(entry_filename, 0, sizeof(entry_filename));
+    get_filename(actual_dir[i], entry_filename, (actual_dir[i].attributes & 0x10) > 0);
+    for (int i = 0; i < strlen(entry_filename); i++)
     {
-      char entry_filename[13];
-      memset(entry_filename, 0, sizeof(entry_filename));
-      get_filename(*entry, entry_filename, (entry->attributes & 0x10) > 0);
-      /* for (int i = 0; i < 12; i++)
-      {
-        printf("%c-%c ", filename[i], entry_filename[i]);
-      }*/
-      if (strcmp(filename, entry_filename) == 0)
-      {
-        return true;
-      }
+      //printf("|%c-%c|", filename[i], entry_filename[i]);
+    }
+    printf("\n");
+    if (strcmp(filename, entry_filename) == 0)
+    {
+      *entry = (actual_dir[i]);
+      return true;
     }
   }
   return false;
@@ -111,7 +117,7 @@ void move_to_cluster(int cluster)
   int cluster_data = end_of_root_dir + ((cluster - 2) * bs.sectors_per_cluster) * bs.sector_size;
   fseek(in, cluster_data, SEEK_SET); //set from start
 }
-void read_file(char *filename)
+void read_file(char *filename, bool save)
 {
   Fat16Entry entry;
   if (!get_entry_by_name(filename, &entry))
@@ -128,7 +134,11 @@ void read_file(char *filename)
   int cluster_left = size_of_cluster;
 
   unsigned char buffer[255];
-  FILE *fp = fopen(filename, "w+");
+  FILE *fp = NULL;
+  if (save)
+  {
+    fp = fopen(filename, "w+");
+  }
   while (file_left > 0 && cluster != 0xFFFF)
   {
     int bytes_to_read = sizeof(buffer);
@@ -138,11 +148,17 @@ void read_file(char *filename)
       bytes_to_read = cluster_left;
 
     int bytes_read = fread(buffer, 1, bytes_to_read, in);
-    for (int i = 0; i < bytes_read; i++)
+    if (!save)
     {
-      printf("%c", buffer[i]);
+      for (int i = 0; i < bytes_read; i++)
+      {
+        printf("%c", buffer[i]);
+      }
     }
-    fwrite(buffer, bytes_read, sizeof(char), fp);
+    else
+    {
+      fwrite(buffer, bytes_read, sizeof(char), fp);
+    }
 
     cluster_left -= bytes_read;
     file_left -= bytes_read;
@@ -153,6 +169,95 @@ void read_file(char *filename)
       move_to_cluster(next_cluster);
       cluster = next_cluster;
       cluster_left = size_of_cluster;
+    }
+  }
+  if (save)
+  {
+    printf("saved to %s \n", filename);
+    fclose(fp);
+  }
+}
+void chdir(char *directory)
+{
+  Fat16Entry entry;
+  if (strcmp(directory, ".") == 0)
+  {
+    return;
+  }
+  memset(&entry, 0, sizeof(Fat16Entry));
+  if (!get_entry_by_name(directory, &entry))
+  {
+    printf("file not found!\n");
+    return;
+  }
+  unsigned short cluster = entry.starting_cluster;
+  if (cluster == 0x00)
+  {
+    load_initial_dir();
+    return;
+  }
+  move_to_cluster(cluster);
+
+  int size_of_cluster = bs.sectors_per_cluster * bs.sector_size;
+  int cluster_left = size_of_cluster;
+
+  if (actual_dir != NULL)
+  {
+    free(actual_dir);
+    entries = 0;
+  }
+  actual_dir = malloc(bs.sectors_per_cluster * bs.sector_size);
+  int clusters = 1;
+
+  while (cluster != 0xFFFF)
+  {
+    Fat16Entry dir;
+    memset(&dir, 0, sizeof(Fat16Entry));
+    int bytes_to_read = sizeof(Fat16Entry);
+    if (sizeof(Fat16Entry) > cluster_left)
+      bytes_to_read = cluster_left;
+
+    int bytes_read = fread(&dir, 1, bytes_to_read, in);
+    if (dir.filename[0] == 0x00)
+    {
+      break;
+    }
+    if (dir.filename[0] != 0xe5)
+    {
+      //printf("%s \n", dir.filename);
+      actual_dir[entries++] = dir;
+    }
+
+    cluster_left -= bytes_read;
+
+    if (cluster_left == 0)
+    {
+      clusters++;
+      actual_dir = realloc(actual_dir, clusters * (bs.sectors_per_cluster * bs.sector_size));
+      unsigned short next_cluster = fat[cluster];
+      move_to_cluster(next_cluster);
+      cluster = next_cluster;
+      cluster_left = size_of_cluster;
+    }
+  }
+}
+void load_initial_dir()
+{
+  int start_of_root_dir = get_start_of_root_dir();
+  fseek(in, start_of_root_dir, SEEK_SET); //set from start
+  if (actual_dir != NULL)
+  {
+    free(actual_dir);
+    entries = 0;
+  }
+  actual_dir = malloc(bs.root_dir_entries * sizeof(Fat16Entry));
+  for (int i = 0; i < bs.root_dir_entries; i++)
+  {
+    Fat16Entry entry;
+    fread(&entry, sizeof(Fat16Entry), 1, in);
+    if (entry.filename[0] != 0x00 && entry.filename[0] != 0xe5)
+    {
+      actual_dir[entries++] = entry;
     }
   }
 }
@@ -182,27 +287,23 @@ void init_fs()
   // Seek to the beginning of root directory, it's position is fixed
   fseek(in, (bs.fat_size_sectors * (bs.number_of_fats - 1)) * bs.sector_size, SEEK_CUR);
   printf("FS initialized\n");
+
+  load_initial_dir();
 }
 
 void dir()
 {
-
-  int start_of_root_dir = get_start_of_root_dir();
-  fseek(in, start_of_root_dir, SEEK_SET); //set from start
-
-  Fat16Entry entry;
   // Read all entries of root directory
   printf("Filesystem root directory listing\n-----------------------\n");
   int files = 0;
   int dirs = 0;
   int file_size = 0;
-  for (int i = 0; i < bs.root_dir_entries; i++)
+  for (int i = 0; i < entries; i++)
   {
-    fread(&entry, sizeof(entry), 1, in);
     // Skip if filename was never used, see http://www.tavi.co.uk/phobos/fat.html#file_attributes
-    if (entry.filename[0] != 0x00)
+    if (actual_dir[i].filename[0] != 0x00)
     {
-      if (print_file(entry, &file_size))
+      if (print_file(actual_dir[i], &file_size))
       {
         dirs++;
       }
@@ -224,13 +325,84 @@ int main()
   dir();
 
   printf("\n\nreading file ABSTRAKT.TXT:\n\n");
-  read_file("ABSTRAKT.TXT");
+  read_file("ABSTRAKT.TXT", false);
 
   printf("\n\nreading file TEST.TXT:\n\n");
-  read_file("TEST.TXT");
+  read_file("TEST.TXT", false);
 
   printf("\n\nreading file FAT16.JPG:\n\n");
-  read_file("FAT16.JPG");
+  read_file("FAT16.JPG", true);
+
+  printf("\n\nchanging to directory ADR1:\n\n");
+  chdir("ADR1");
+
+  printf("\n\nreading directory:\n\n");
+  dir();
+
+  printf("\n\nreading file KOCKA.JPG:\n\n");
+  read_file("KOCKA.JPG", true);
+
+  printf("\n\nreading file VYUKA.JPG:\n\n");
+  read_file("VYUKA.JPG", true);
+
+  printf("\n\nchanging to directory .:\n\n");
+  chdir(".");
+
+  printf("\n\nchanging to directory parent:\n\n");
+  chdir("..");
+  dir();
+
+  printf("\n\nchanging to directory parent:\n\n");
+  chdir("ADR2");
+  dir();
+
+  printf("\n\nreading file HISTORIE.TXT:\n\n");
+  read_file("HISTORIE.TXT", false);
+
+  printf("\n\nreading file KOREN.TXT:\n\n");
+  read_file("KOREN.TXT", false);
+
+  char line[1024];
+  while (true)
+  {
+    memset(line, 0, sizeof(line));
+    printf("> ");
+    fgets(line, sizeof(line), stdin);
+    char *pos;
+    if ((pos = strchr(line, '\n')) != NULL)
+    {
+      *pos = '\0';
+    }
+    char *cmd = NULL;
+    char *arg = NULL;
+    if (cmd = strtok(line, " "))
+    {
+      if (strcmp(cmd, "chdir") == 0)
+      {
+        arg = strtok(NULL, " ");
+        chdir(arg);
+      }
+      else if (strcmp(cmd, "dir") == 0)
+      {
+        dir();
+      }
+      else if (strcmp(cmd, "exit") == 0)
+      {
+        return (0);
+      }
+      else if (strcmp(cmd, "read") == 0)
+      {
+        arg = strtok(NULL, " ");
+        read_file(arg, false);
+      }
+      else if (strcmp(cmd, "save") == 0)
+      {
+        arg = strtok(NULL, " ");
+        read_file(arg, true);
+      }
+    }
+  }
+
   fclose(in);
   return 0;
 }
